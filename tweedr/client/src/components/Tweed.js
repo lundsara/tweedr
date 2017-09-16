54c4e2b7aeb4558bd657b31c5fd1f244d5b76719
Chris Li <licmike75@gmail.com>
2017-09-16 18:58:31 -0400

    fixed timeConversion function by adding date method

--- tweedr/client/src/components/Tweed.js
+++ tweedr/client/src/components/Tweed.js
@@ -3,34 +3,18 @@ import React, { Component } from 'react';
 
 class Tweed extends Component {
     
-    // https://stackoverflow.com/questions/175554/how-to-convert-milliseconds-into-human-readable-form
-conversion (millisecs) {
+timeConversion (millisecs) {
     let mili = parseInt(millisecs,10);
     let date = new Date(mili);
-    let days = date.getUTCDate()
-    let hours = date.getUTCHours()
-    let mins = date.getUTCMinutes()
-    let sec = date.getUTCSeconds()
-    let str = `${days}th of Sept`;
-    // str += date.getUTCDate()-1 + " days, ";
-    // str += date.getUTCHours() + " hours, ";
-    // str += date.getUTCMinutes() + " minutes, ";
-    // str += date.getUTCSeconds() + " seconds ";
-    // console.log(millisecs)
-    // let parString = parseInt(str,10)
-    // console.log(`This is inside the conversion function ${typeof mins}`)
-    // console.log(typeof str);
-    // console.log(str);
-    return str
+    let n = date.toLocaleString();
+    return n
 }
-
-// let newTime = conversion(this.props.tweed.tweed_time)
 
 render() {
     console.log('Tweeds rendering');
     return(
         <div className="tweed">
-            <h2>{this.props.tweed.tweed_text} - {this.conversion(this.props.tweed.tweed_time)}</h2>
+            <h2>{this.props.tweed.tweed_text} - {this.timeConversion(this.props.tweed.tweed_time)}</h2>
         </div>
         )
     }
