a02999e8afb927cbed72174571c8f35a7289399a
sarah lund <sarah.christine.lund@gmail.com>
2017-10-19 13:03:33 -0400

    make it better

--- tweedr/db/migrations/migration_05132017.sql
+++ tweedr/db/migrations/migration_05132017.sql
@@ -1,5 +1,3 @@
-\c tweedr_dev
-
 CREATE TABLE IF NOT EXISTS tweeds (
   id BIGSERIAL PRIMARY KEY,
   tweed_text VARCHAR(1024),
---
+++ tweedr/db/seeds/tweeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO tweeds (tweed_text, tweed_time) VALUES
+  ('Hello World!', 1494788500041),
+  ('I love using Tweedr... so much better than twitter.', 1494788543350),
+  ('React is the best!', 1494788564011);
